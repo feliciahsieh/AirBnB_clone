eebd226887d216a53de519d6691c2cdd48e7e37b
feliciahsieh <felicia@tixwriteoff.com>
2017-09-29 02:32:04 +0000

    in progress. Not working

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,14 @@
 """
 File: base_model.py
 """
-import json
-
+import uuid
+import datetime
 
 class BaseModel:
     """
     BaseModel definition for AirBnB clone project
     """
-    def __init__(self, id=None):
+    def __init__(self):
         """
         __init__ - initialze BaseModel object
         Args:
@@ -17,13 +17,9 @@ class BaseModel:
         Return:
             None
         """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-        # self.created_at = x
-        # self.updated_at = x
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """
@@ -33,16 +29,16 @@ class BaseModel:
         Return:
             None
         """
-        # print("[{}] ({}) {}".format(class, self.id, self.__dict__))
+        # print("[{}] ({}) {}".format(self.__name__.__class__, self.id, self.__dict__))
     def save(self):
         """
-        save - updates public instance attribute, udpated_at, and timestamp
+        save - updates public instance attribute, updated_at, and timestamp
         Args:
             None
         Return:
             None
         """
-        #
+        # self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """
@@ -52,7 +48,7 @@ class BaseModel:
         Return:
             entire dictionary of instance
         """
-        # self.__class__ = ?BaseModel
-        # self.created_at =
-        # self.updated_at =
-        # return self.__dict__
+        # self.__class__.__name__ = ?BaseModel
+        # self.created_at.isoformat()
+        # self.updated_at.isoformat()
+        # return self.__dict__.copy
