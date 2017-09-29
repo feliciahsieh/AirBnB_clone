152f35722151c1683e4561852893ea1c8ff33204
MinasA1 <minasanton@gmail.com>
2017-09-29 21:39:40 +0000

    console 0.0.1 ready

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Command Intrpreter - Console"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """class HBNBCommand"""
+    def do_quit(self, args):
+        """Quit command to exit the program."""
+        raise SystemExit
+
+    def do_EOF(self, args):
+        """EOF command to exit the program."""
+        raise SystemExit
+
+if __name__ == '__main__':
+    prompt = HBNBCommand()
+    prompt.prompt = '(hbnb)'
+    prompt.cmdloop('')
