d2dbb657d64d4ba7d58b59da25901c558422524f
feliciahsieh <felicia@tixwriteoff.com>
2017-09-29 16:46:53 +0000

    In __init__(), changed self.updated_at to use the now() method. In save() method, removed the stringify for self.updated_at

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,7 @@ class BaseModel:
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """
@@ -43,7 +43,7 @@ class BaseModel:
         Return:
             None
         """
-        self.updated_at = str(datetime.datetime.now())
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """
