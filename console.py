f81ad9c812b80ce32e581d09ee6a29c26112dded
feliciahsieh <felicia@tixwriteoff.com>
2017-09-30 23:41:49 +0000

    added comments

--- console.py
+++ console.py
@@ -4,13 +4,28 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """class HBNBCommand"""
+    """
+    class HBNBCommand - console for the AirBnB clone
+    """
+
     def do_quit(self, args):
-        """Quit command to exit the program."""
+        """
+        do_quit - Quit command to exit the program
+        Args:
+            args(tuple) - arguments sent with the quit command
+        Return:
+            None
+        """
         raise SystemExit
 
     def do_EOF(self, args):
-        """EOF command to exit the program."""
+        """
+        do_EOF - EOF command to exit the program
+        Args:
+            None
+        Return:
+            None
+        """
         raise SystemExit
 
 if __name__ == '__main__':
