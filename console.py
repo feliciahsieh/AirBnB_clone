6629d11988726846ae6fa3ead6b4f873f0c334f1
feliciahsieh <felicia@tixwriteoff.com>
2017-10-01 00:57:21 +0000

    #7 create() seems to work

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Command Interpreter - Console"""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -8,9 +9,8 @@ class HBNBCommand(cmd.Cmd):
     class HBNBCommand - console for the AirBnB clone
         Note: "help <cmd>" functionality is provided by cmd module
     """
-    intro = 'Welcome to the AirBnB clone. Type help or ? to list commands.\n'
-    prompt = '(hbnb) '
 
+    # ----- basic Holberton AirBnB commands -----
     def do_quit(self, args):
         """Quit command to exit the program
         """
@@ -26,7 +26,28 @@ class HBNBCommand(cmd.Cmd):
         """
         pass
 
+    # ----- basic CRUD commands -----
+    def do_create(self, arg):
+        """Create BaseModel instance"""
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            bm = BaseModel()
+            bm.save()
+            print('{}'.format(bm.id))
+
+    def do_show(self):
+        """Show object"""
+
+    # def do_destroy(self):
+    # def do_all(self):
+    # def do_update(self):
+
 if __name__ == '__main__':
+
+    intro = '** Welcome to the AirBnB clone. Type help or ? to list commands **'
     prompt = HBNBCommand()
     prompt.prompt = '(hbnb) '
-    prompt.cmdloop()
+    prompt.cmdloop(intro)
