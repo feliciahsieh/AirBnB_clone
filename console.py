25cc5037150fe780d7aca516f2c1cebdfc58cad0
feliciahsieh <felicia@tixwriteoff.com>
2017-10-02 17:44:31 +0000

    added some #7 logic. Not done

--- console.py
+++ console.py
@@ -20,7 +20,7 @@ class HBNBCommand(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.prompt = "(hbnb) "
-        self.intro = "** Welcome to the AirBnB clone. Type help to list cmds **"
+        self.intro = "* Welcome to the AirBnB clone. Type help to list cmds *"
 
     # ----- basic AirBnB clone commands -----
     def do_quit(self, args):
@@ -83,7 +83,7 @@ class HBNBCommand(cmd.Cmd):
             # destroy object # missing logic
             # save to file
             # else:
-                # print("** no instance found **")
+            #     print("** no instance found **")
 
     def do_all(self, arg):
         """Show all objects"""
@@ -93,12 +93,28 @@ class HBNBCommand(cmd.Cmd):
         elif (len(input) == 1 and input[0] == "BaseModel"):
             allObjs = storage.all()
             # if input[0] in allObjs.keys(): # missing logic
-            print(allObjs) # missing logic
+            print(allObjs)  # missing logic
         else:
             print("** class doesn't exist **")
 
-    # def do_update(self, arg):
-    # something
+    def do_update(self, arg):
+        """Update obj atribute with info
+        """
+        input = arg.split()
+        if len(input) == 0:
+            print("** class name missing **")
+        elif input[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(input) == 1:
+            print("** instance id missing **")
+        elif input[] not in { "1234-1234-1234" }: # missing logic
+            print("** no instance found **")
+        elif len(input) == 2:
+            print("** attribute name missing **")
+        elif input[2] not in allObjs.keys(): # missing logic
+            print("** value missing **")
+        elif len(input) > 4:
+            # don't execute
 
 if __name__ == '__main__':
     console = HBNBCommand()
