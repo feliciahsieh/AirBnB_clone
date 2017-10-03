afedeecd5cb36d2014c3471aa0a3b930470133c9
MinasA1 <minasanton@gmail.com>
2017-10-03 03:04:37 +0000

    updated __init__py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """init file"""
 from models.engine.file_storage import FileStorage
-__all__ = ["base_model", "user"]
+__all__ = ["base_model", "user", "state", "city", "amenity", "place", "review"]
 storage = FileStorage()
 storage.reload()
